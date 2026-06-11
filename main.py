a1fd07500e7fc32c79da764fc84b935d7a39d7e5
Phạm Trần Vũ Thái <thaipham1417@gmail.com>
2026-06-11 14:34:51 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def tinh_diem_gpa(diem_so):
-    # TODO: Cần cập nhật logic quy đổi sang hệ 4
-    return 0.0
+def tinh_thue_thu_nhap(thu_nhap):
+    # TODO: Sinh viên sẽ cập nhật công thức tính thuế ở đây
+    return 0
 
-print("Điểm GPA hệ 4 là:", tinh_diem_gpa(8.5))
+print("Thuế phải nộp của bạn là:", tinh_thue_thu_nhap(10000000))
