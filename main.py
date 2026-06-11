e1f5bd03216f33a521cfee746e648481577d626c
Pham Tran Vu Thai <thaipham1417@gmail.com>
2026-06-11 14:02:39 +0700

    feat: dev A

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
 def tinh_thue_thu_nhap(thu_nhap):
-    # TODO: Sinh viên sẽ cập nhật công thức tính thuế ở đây
-    return 0
-
-print("Thuế phải nộp của bạn là:", tinh_thue_thu_nhap(10000000))
+    thu_nhap_tinh_thue = thu_nhap - 4000000
+    return max(0, thu_nhap_tinh_thue * 0.05)
